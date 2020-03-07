df60efa40aff96e72b5153704f157df3d658f418
LeeJY <grrcm@hanmail.net>
2020-03-07 12:03:12 +0900

    최초작성

---
+++ src/test/kotlin/com/colin/example/simpletodolist/SimpleTodoListApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.colin.example.simpletodolist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleTodoListApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
