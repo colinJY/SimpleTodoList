54317f148aea498ddd12b551957b29833bcd75ad
LeeJY <grrcm@hanmail.net>
2020-03-07 20:40:54 +0900

    유용한 링크 추가.

--- src/main/kotlin/com/colin/example/simpletodolist/todo/TodoController.kt
+++ src/main/kotlin/com/colin/example/simpletodolist/todo/TodoController.kt
@@ -3,15 +3,20 @@ package com.colin.example.simpletodolist.todo
 import com.colin.example.simpletodolist.todo.dto.InsertTodoRequestDto
 import com.colin.example.simpletodolist.todo.dto.InsertTodoResponseDto
 import com.colin.example.simpletodolist.todo.service.TodoService
+import org.springframework.validation.Errors
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 class TodoController(val todoService: TodoService) {
 
     @PostMapping("/todo")
-    fun insert(@RequestBody insertTodoRequestDto: InsertTodoRequestDto) : InsertTodoResponseDto{
+    fun insert(
+            @RequestBody @Valid insertTodoRequestDto: InsertTodoRequestDto,
+            errors: Errors
+    ) : InsertTodoResponseDto{
 
         return todoService.insertPost(insertTodoRequestDto)
     }
--- src/main/kotlin/com/colin/example/simpletodolist/todo/dto/InsertTodoRequestDto.kt
+++ src/main/kotlin/com/colin/example/simpletodolist/todo/dto/InsertTodoRequestDto.kt
@@ -4,9 +4,12 @@ import com.colin.example.simpletodolist.todo.domain.Todos
 import javax.validation.constraints.*
 
 data class InsertTodoRequestDto(
-        @get:NotBlank @get:Size(max = 200)
+        @get:NotBlank(message = "제목을 입력하여 주십시오.")
+        @get:Size(max = 200, message = "최대 200byte까지 입력 가능합니다.")
         val title: String,
-        @get:NotBlank @get:Size(max = 2000)
+
+        @get:NotBlank(message = "내용을 입력하여 주십시오.")
+        @get:Size(max = 2000, message = "최대 2000byte까지 입력 가능합니다.")
         val content: String
 ) {
     fun toEntity(title: String, content: String): Todos = Todos(title, content)
