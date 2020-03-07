5dba29ec57172ac83a6a396cd2d6183573821bbb
LeeJY <grrcm@hanmail.net>
2020-03-07 20:04:05 +0900

    bean validation 추가

--- src/main/kotlin/com/colin/example/simpletodolist/todo/dto/InsertTodoRequestDto.kt
+++ src/main/kotlin/com/colin/example/simpletodolist/todo/dto/InsertTodoRequestDto.kt
@@ -1,9 +1,12 @@
 package com.colin.example.simpletodolist.todo.dto
 
 import com.colin.example.simpletodolist.todo.domain.Todos
+import javax.validation.constraints.*
 
 data class InsertTodoRequestDto(
+        @get:NotBlank @get:Size(max = 200)
         val title: String,
+        @get:NotBlank @get:Size(max = 2000)
         val content: String
 ) {
     fun toEntity(title: String, content: String): Todos = Todos(title, content)
