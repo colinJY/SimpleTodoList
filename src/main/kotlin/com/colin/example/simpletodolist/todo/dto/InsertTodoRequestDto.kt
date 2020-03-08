aa451d6c1ae3a7da8ac786458e66c04019bf5766
LeeJY <grrcm@hanmail.net>
2020-03-08 11:25:29 +0900

    request dto에 기본 값을 설정하여, valid 검증 가능하도록 수정.

--- src/main/kotlin/com/colin/example/simpletodolist/todo/dto/InsertTodoRequestDto.kt
+++ src/main/kotlin/com/colin/example/simpletodolist/todo/dto/InsertTodoRequestDto.kt
@@ -6,11 +6,11 @@ import javax.validation.constraints.*
 data class InsertTodoRequestDto(
         @get:NotBlank(message = "제목을 입력하여 주십시오.")
         @get:Size(max = 200, message = "최대 200byte까지 입력 가능합니다.")
-        val title: String,
+        val title: String = "",
 
         @get:NotBlank(message = "내용을 입력하여 주십시오.")
         @get:Size(max = 2000, message = "최대 2000byte까지 입력 가능합니다.")
-        val content: String
+        val content: String = ""
 ) {
     fun toEntity(): Todos = Todos(title, content)
 }
